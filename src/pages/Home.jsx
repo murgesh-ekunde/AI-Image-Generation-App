177ec41154c255fc9caedf4c01ff75f2a5ab6082
Murgesh Ekunde <mekunde@gmail.com>
2023-02-13 21:53:20 +0530

    link updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch(`${API}/api/v1/post`, {
+      const response = await fetch(`https://ai-image-generation-app-backend.onrender.com/api/v1/post`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
