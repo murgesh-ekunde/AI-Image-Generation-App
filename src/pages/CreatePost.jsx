b02379dc90bae912ef1be601f3797d428423f211
Murgesh Ekunde <mekunde@gmail.com>
2023-02-13 22:07:58 +0530

    link bug solved

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
+import { API } from '../../global';
 
 const CreatePost = () => {
   const navigate = useNavigate();
